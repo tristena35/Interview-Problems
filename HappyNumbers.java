31f7fe524bc4e1b297f827dc10e01e456aad4317
Tristen Aguilar <tristenaguilar24@gmail.com>
2019-10-09 18:48:17 -0400

    Update HappyNumbers.java

--- HappyNumbers.java
+++ HappyNumbers.java
@@ -6,6 +6,7 @@ public class HappyNumbers {
         int sum = 0;
         int y = 0;
         
+        //Here I set the cap at 100 just so it does not check forever
         while(y < 100){
             
         	System.out.println("n = " + n);
@@ -13,7 +14,9 @@ public class HappyNumbers {
         	
             while(n > 0){
             	            
-                sum += (n % 10) * (n % 10);      
+            	//Here we take the last digit of each sum and use the algorithm to find the new sum
+                sum += (n % 10) * (n % 10);
+                          
                 n /= 10;
             
                 System.out.println("Sum = " + sum);
@@ -22,16 +25,16 @@ public class HappyNumbers {
             
             System.out.println("Next");
             
+            //If sum equals to 1 then you found a happy number
             if(sum == 1)
                 return true;
             else
                 n = sum;
         
-            y ++;
+            y++;
             
         }
-
-	return false;
+		return false;
         
    }
     
