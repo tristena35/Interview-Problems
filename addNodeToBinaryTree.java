71f3632bece0b5da4bd49f539a01aad1eec1a914
Tristen Aguilar <tristenaguilar24@gmail.com>
2019-11-20 13:27:04 -0500

    Add To a Binary Search Tree

---
+++ addNodeToBinaryTree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        
+        //Base case: If you are in null node, you have reached your destination
+        if(root == null){
+            TreeNode tempNode = new TreeNode(val);
+            root = tempNode;
+            return root;
+        }
+        
+        //If the value is greater than the current root, traverse right
+        if(val > root.val) root.right = insertIntoBST(root.right, val);
+        
+        //If the value is less than the current root, traverse left
+        if(val < root.val) root.left = insertIntoBST(root.left, val);
+        
+        return root;
+        
+    }
+    
+    
+    /*
+    Go through the tree recursively,
+    if the node you are currently traversing is null, you may want to traverse
+    Go through the left or right subtree depending on which order side is appropriate
+    less than go left, bigger than go right
+    
+    */
+    
+    
+}
