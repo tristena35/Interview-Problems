853292b47294b1c9acdba242ae35d66cb7df1bbe
tristena35 <tristenaguilar24@gmail.com>
2020-03-28 13:43:49 -0400

    Updating this file

--- smallerThanTheCurrentNumber.java
+++ smallerThanTheCurrentNumber.java
@@ -28,3 +28,44 @@ class Solution {
     }
                                    
 }
+
+
+
+/*
+
+
+
+                OR SOLUTION USING A HASHMAP
+
+
+
+*/
+
+
+
+class Solution {
+    
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+    
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] numsClone = nums.clone();
+        Arrays.sort(nums);
+
+        for(int i = 0; i< nums.length; i++){
+            
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i], i);
+        
+        }
+        
+        int[] ansArray = new int[nums.length];
+        
+        for(int i = 0; i < numsClone.length; i++)
+            ansArray[i] = map.get(numsClone[i]);
+        
+        
+        return ansArray;
+
+    }
+                                   
+}
